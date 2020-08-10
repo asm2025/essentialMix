3239fc582ec668d645995ab0712d3db811e137ae
Ahmed Assem <asm2025@outlook.com>
2020-08-10 02:57:41 +0200

    Added Guid Combine

--- Legacy/asm.Media/Properties/AssemblyInfo.cs
+++ Legacy/asm.Media/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("asm")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyDescription("FFmpeg wrapper")]
-[assembly: AssemblyFileVersion("20.8.9.0")]
+[assembly: AssemblyFileVersion("20.8.10.0")]
 [assembly: AssemblyProduct("asm.MediaToolkit")]
 [assembly: AssemblyTitle("asm.MediaToolkit")]
 [assembly: AssemblyVersion("10.0.0.0")]
