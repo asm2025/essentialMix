3b9eb377ac3e7ba33dfe7f30aec1c4bd2ff23ec5
asm <ahmed.assem@flairstech.com>
2020-11-07 12:43:16 +0200

    EllipsesHelper. NetworkCredentials helper and extension. Service helper.

---
+++ Standard/asm.Exceptions/Security/ImpersonationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using JetBrains.Annotations;
+
+namespace asm.Exceptions.Security
+{
+	public class ImpersonationException : Exception
+	{
+		private readonly Win32Exception _win32Exception;
+
+		/// <inheritdoc />
+		public ImpersonationException([NotNull] Win32Exception win32Exception)
+			: base(win32Exception.Message, win32Exception)
+		{
+			_win32Exception = win32Exception;
+		}
+
+		public int ErrorCode => _win32Exception.ErrorCode;
+
+		public int NativeErrorCode => _win32Exception.NativeErrorCode;
+	}
+}
---
+++ Standard/asm/Extensions/NetworkCredentialExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using asm.Helpers;
+using asm.Security;
+using JetBrains.Annotations;
+using Microsoft.Win32.SafeHandles;
+
+namespace asm.Extensions
+{
+	public static class NetworkCredentialExtension
+	{
+		[NotNull]
+		public static SafeAccessTokenHandle Impersonate([NotNull] this NetworkCredential thisValue, LogonType logonType)
+		{
+			return NetworkCredentialHelper.Impersonate(thisValue, logonType);
+		}
+	}
+}
