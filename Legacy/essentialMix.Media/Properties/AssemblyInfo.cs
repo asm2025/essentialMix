58446490cae0614675c631615a14fc64dcf38d8c
asm <asm2025@outlook.com>
2021-03-16 02:57:44 +0200

    Publish nuget packages. Remove unrelated projects and cleanup

--- Legacy/essentialMix.Media/Properties/AssemblyInfo.cs
+++ Legacy/essentialMix.Media/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.7.0.0")]
-[assembly: AssemblyFileVersion("21.3.15.17")]
+[assembly: AssemblyVersion("12.21.0.0")]
+[assembly: AssemblyFileVersion("21.3.16.3")]
