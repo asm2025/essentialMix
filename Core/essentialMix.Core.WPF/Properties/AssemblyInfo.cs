496f06c601b64403b328e5a5f9b3daede71597cf
asm <asm2025@outlook.com>
2021-04-07 03:13:57 +0200

    Update and adjust versions

--- Core/essentialMix.Core.WPF/Properties/AssemblyInfo.cs
+++ Core/essentialMix.Core.WPF/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 
 [assembly: ThemeInfo(
 	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -10,13 +8,3 @@ using System.Windows;
 											  //(used if a resource is not found in the page,
 											  // app, or any theme specific resource dictionaries)
 )]
-
-[assembly: ComVisible(false)]
-[assembly: AssemblyCompany("asm")]
-[assembly: AssemblyDescription("Common extensions methods, helper classes, components, common patterns, Windows Presentation Foundation, WPF")]
-[assembly: AssemblyFileVersion("21.4.7.7")]
-[assembly: AssemblyInformationalVersion("12.27.0.0")]
-[assembly: AssemblyProduct("essentialMix")]
-[assembly: AssemblyTitle("essentialMix")]
-[assembly: AssemblyVersion("12.27.0.0")]
-[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/asm2025/essentialMix")]
