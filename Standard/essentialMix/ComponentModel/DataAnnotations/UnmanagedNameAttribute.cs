cfb793039eba9d5c9b8220c7ff7593ab7df8c86a
asm <ahmed.assem@flairstech.com>
2021-08-27 10:12:12 +0200

    Add UnmanagedNameAttribute Enhance ObjectHelper and add ObjectHelper.MarshalDispose

---
+++ Standard/essentialMix/ComponentModel/DataAnnotations/UnmanagedNameAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ATCommon.Extensions;
+using JetBrains.Annotations;
+
+namespace ATCommon.ComponentModel.DataAnnotations
+{
+	[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Struct | AttributeTargets.Class)]
+	public class UnmanagedNameAttribute : Attribute
+	{
+		private readonly string _name;
+
+		public UnmanagedNameAttribute([NotNull] string name)
+		{
+			name = name.ToNullIfEmpty() ?? throw new ArgumentNullException(nameof(name));
+			_name = name;
+		}
+
+		public override string ToString()
+		{
+			return _name;
+		}
+	}
+}
--- Standard/essentialMix/Helpers/ObjectHelper.cs
+++ Standard/essentialMix/Helpers/ObjectHelper.cs
@@ -1,20 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-namespace essentialMix.Helpers
+namespace ATCommon.Helpers
 {
 	public static class ObjectHelper
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.ForwardRef)]
 		public static void Dispose<T>(ref T value)
 			where T : IDisposable
 		{
-			try
-			{
-				value?.Dispose();
-			}
-			catch (ObjectDisposedException)
-			{
-				// ignored
-			}
+			Dispose(value);
 			value = default(T);
 		}
 
@@ -31,11 +28,33 @@ namespace essentialMix.Helpers
 			}
 		}
 
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.ForwardRef)]
+		public static void MarshalDispose(ref object value)
+		{
+			MarshalDispose(value);
+			value = null;
+		}
+
+		public static void MarshalDispose(object value)
+		{
+			if (value is null) return;
+
+			if (Marshal.IsComObject(value))
+			{
+				int ret = Marshal.ReleaseComObject(value);
+				Debug.Assert(ret >= 0);
+				return;
+			}
+
+			if (value is not IDisposable disposable) return;
+			Dispose(disposable);
+		}
+
 		public static bool IsCompatible<T>(object value)
 		{
 			// Non-null values are fine. Only accept nulls if T is a class or Nullable<U>.
 			// Note that default(T) is not equal to null for value types except when T is Nullable<U>. 
-			return value is T || value == null && default(T) == null;
+			return value is T || value is null && default(T) == null;
 		}
 	}
 }
