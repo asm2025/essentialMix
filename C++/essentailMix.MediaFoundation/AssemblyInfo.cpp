44c7aefd3595c29775c8921e4bc0c894f534c8f0
asm <asm2025@outlook.com>
2021-09-15 23:49:56 +0200

    Add essentialMix.MediaFoundation project (C++)

---
+++ C++/essentailMix.MediaFoundation/AssemblyInfo.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+using namespace System;
+using namespace Reflection;
+using namespace Runtime::CompilerServices;
+using namespace Runtime::InteropServices;
+using namespace Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"essentialMix.MediaFoundation")];
+[assembly:AssemblyDescriptionAttribute(L"Core functionality to stream and capture from a webcam.")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"asm")];
+[assembly:AssemblyProductAttribute(L"essentialMix.MediaFoundation")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2021")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("12.33.0.0")];
+[assembly:AssemblyFileVersionAttribute("21.9.4.0")];
+
+[assembly:ComVisible(false)];
---
+++ C++/essentailMix.MediaFoundation/Net/include/Capture.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using namespace System;
+
+namespace essentialMix::MediaFoundation
+{
+	public ref class Capture
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
