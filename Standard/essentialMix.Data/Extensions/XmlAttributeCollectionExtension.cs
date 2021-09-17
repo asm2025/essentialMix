7683903297733269dcb09abf63f58016dbfcfd6d
asm <ahmed.assem@flairstech.com>
2021-09-17 19:52:48 +0200

    Change Attribute to Get

--- Standard/essentialMix.Data/Extensions/XmlAttributeCollectionExtension.cs
+++ Standard/essentialMix.Data/Extensions/XmlAttributeCollectionExtension.cs
@@ -20,15 +20,33 @@ namespace essentialMix.Extensions
 		}
 
 		[MethodImpl(MethodImplOptions.ForwardRef | MethodImplOptions.AggressiveInlining)]
-		public static string Attribute([NotNull] this XmlAttributeCollection thisValue, [NotNull] string name)
+		public static string Get([NotNull] this XmlAttributeCollection thisValue, [NotNull] string name)
 		{
 			return thisValue[name]?.Value;
 		}
 
 		[MethodImpl(MethodImplOptions.ForwardRef | MethodImplOptions.AggressiveInlining)]
-		public static string Attribute([NotNull] this XmlAttributeCollection thisValue, [NotNull] string localName, string namespaceURI)
+		public static string Get([NotNull] this XmlAttributeCollection thisValue, [NotNull] string localName, string namespaceURI)
 		{
 			return thisValue[localName, namespaceURI]?.Value;
+		}
+
+		[MethodImpl(MethodImplOptions.ForwardRef | MethodImplOptions.AggressiveInlining)]
+		public static T Get<T>([NotNull] this XmlAttributeCollection thisValue, [NotNull] string name, T defaultValue)
+		{
+			XmlAttribute attribute = thisValue[name];
+			return attribute != null
+						? attribute.Value.To(defaultValue)
+						: defaultValue;
+		}
+
+		[MethodImpl(MethodImplOptions.ForwardRef | MethodImplOptions.AggressiveInlining)]
+		public static T Get<T>([NotNull] this XmlAttributeCollection thisValue, [NotNull] string localName, string namespaceURI, T defaultValue)
+		{
+			XmlAttribute attribute = thisValue[localName, namespaceURI];
+			return attribute != null
+						? attribute.Value.To(defaultValue)
+						: defaultValue;
 		}
 	}
 }
