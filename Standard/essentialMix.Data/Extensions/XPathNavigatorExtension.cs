218101e2628b24d899b66fc64c31fdfb82ffaf71
asm <ahmed.assem@flairstech.com>
2021-09-17 19:55:25 +0200

    XPathNavigatorExtension

--- Standard/essentialMix.Data/Extensions/XPathNavigatorExtension.cs
+++ Standard/essentialMix.Data/Extensions/XPathNavigatorExtension.cs
@@ -13,5 +13,40 @@ namespace essentialMix.Extensions
 			XPathNavigator navigator = resolver == null ? thisValue.SelectSingleNode(nodePath) : thisValue.SelectSingleNode(nodePath, resolver);
 			return navigator?.Value;
 		}
+
+		public static string Get([NotNull] this XPathNavigator thisValue, string name, IXmlNamespaceResolver resolver = null) { return Get(thisValue, name, null, resolver); }
+		public static string Get([NotNull] this XPathNavigator thisValue, string name, string defaultValue, IXmlNamespaceResolver resolver = null)
+		{
+			if (string.IsNullOrEmpty(name)) return defaultValue;
+			XPathNavigator attribute = thisValue.SelectSingleNode("@" + name, resolver);
+			return attribute?.Value ?? defaultValue;
+		}
+
+		public static T Get<T>([NotNull] this XPathNavigator thisValue, string name, IXmlNamespaceResolver resolver = null) { return Get(thisValue, name, default(T), resolver); }
+		public static T Get<T>([NotNull] this XPathNavigator thisValue, string name, T defaultValue, IXmlNamespaceResolver resolver = null)
+		{
+			string value = Get(thisValue, name, null, resolver);
+			return string.IsNullOrEmpty(value)
+						? defaultValue
+						: value.To(defaultValue);
+		}
+
+		public static bool TryGet([NotNull] this XPathNavigator thisValue, string name, out string result, IXmlNamespaceResolver resolver = null)
+		{
+			result = null;
+			if (string.IsNullOrEmpty(name)) return false;
+			XPathNavigator attribute = thisValue.SelectSingleNode("@" + name, resolver);
+			if (attribute == null) return false;
+			result = attribute.Value;
+			return true;
+		}
+
+		public static bool TryGet<T>([NotNull] this XPathNavigator thisValue, string name, out T result, IXmlNamespaceResolver resolver = null)
+		{
+			if (TryGet(thisValue, name, out string value, resolver)) return value.TryConvert(out result);
+			result = default(T);
+			return false;
+
+		}
 	}
 }
