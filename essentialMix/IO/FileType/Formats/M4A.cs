cd71b6efae98d93cdbb22ec76a4b14d9187fe050
asm <asm2025@outlook.com>
2022-04-28 22:25:10 +0200

    fix missing files

--- essentialMix/IO/FileType/Formats/M4A.cs
+++
@@ -1,5 +0,0 @@
-﻿using essentialMix.Web;
-
-namespace essentialMix.IO.FileType.Formats;
-
-public record M4a() : IsoBMFFBase("m4a", MediaTypeNames.Audio.Mp4, new byte[] { 0x00, 0x00, 0x00, 0x20 }, new byte[] { 0x4D, 0x34, 0x41 });
