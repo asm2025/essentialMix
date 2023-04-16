710235e7b765786a9f74539d039788a651c4fe7a
asm <asm2025@outlook.com>
2023-04-16 02:43:12 +0200

    add princible extensions version change

---
+++ essentialMix/Extensions/IPrincipalExtension.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using JetBrains.Annotations;
+
+namespace essentialMix.Extensions;
+
+public static class IPrincipalExtension
+{
+	public static bool IsInRole([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRole(role1, role2);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRole(role1, role2)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRole(role1, role2, role3);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRole(role1, role2, role3)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRole(role1, role2, role3, role4);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRole(role1, role2, role3, role4)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4, [NotNull] string role5)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRole(role1, role2, role3, role4, role5);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRole(role1, role2, role3, role4, role5)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this IPrincipal thisValue, [NotNull] IEnumerable<string> roles)
+	{
+		ICollection<string> collection = roles as ICollection<string> ?? new HashSet<string>(roles);
+		if (collection.Count == 0) return false;
+
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRole(collection);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRole(collection)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] string role)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(role);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(role)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(role1, role2);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(role1, role2)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(role1, role2, role3);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(role1, role2, role3)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(role1, role2, role3, role4);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(role1, role2, role3, role4)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4, [NotNull] string role5)
+	{
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(role1, role2, role3, role4, role5);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(role1, role2, role3, role4, role5)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this IPrincipal thisValue, [NotNull] IEnumerable<string> roles)
+	{
+		ICollection<string> collection = roles as ICollection<string> ?? new HashSet<string>(roles);
+		if (collection.Count == 0) return false;
+
+		if (thisValue is not ClaimsPrincipal claimsPrincipal)
+			return thisValue.Identity is ClaimsIdentity claimsIdentity && claimsIdentity.IsInRoleEx(collection);
+
+		foreach (ClaimsIdentity identity in claimsPrincipal.Identities)
+		{
+			if (!identity.IsInRoleEx(collection)) continue;
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ essentialMix/Extensions/IdentityExtension.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using JetBrains.Annotations;
+
+namespace essentialMix.Extensions;
+
+public static class ClaimsIdentityExtension
+{
+	public static bool IsInRole([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2)
+	{
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)) return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3)
+	{
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase)) return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4)
+	{
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role4, StringComparison.OrdinalIgnoreCase)) return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4, [NotNull] string role5)
+	{
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role4, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role5, StringComparison.OrdinalIgnoreCase)) return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRole([NotNull] this ClaimsIdentity thisValue, [NotNull] IEnumerable<string> roles)
+	{
+		ICollection<string> collection = roles as ICollection<string> ?? new HashSet<string>(roles);
+		if (collection.Count == 0) return false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (!collection.Contains(claim.Value)) continue;
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] string role)
+	{
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role, StringComparison.OrdinalIgnoreCase))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2)
+	{
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3)
+	{
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4)
+	{
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role4, StringComparison.OrdinalIgnoreCase))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] string role1, [NotNull] string role2, [NotNull] string role3, [NotNull] string role4, [NotNull] string role5)
+	{
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (string.Equals(claim.Value, role1, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role2, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role3, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role4, StringComparison.OrdinalIgnoreCase)
+				|| string.Equals(claim.Value, role5, StringComparison.OrdinalIgnoreCase))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+
+	public static bool IsInRoleEx([NotNull] this ClaimsIdentity thisValue, [NotNull] IEnumerable<string> roles)
+	{
+		ICollection<string> collection = roles as ICollection<string> ?? new HashSet<string>(roles);
+		if (collection.Count == 0) return false;
+
+		bool isInRole = false;
+		bool isInOthers = false;
+
+		foreach (Claim claim in thisValue.Claims.Where(e => e.Type == thisValue.RoleClaimType))
+		{
+			if (collection.Contains(claim.Value))
+			{
+				isInRole = true;
+			}
+			else
+			{
+				isInOthers = true;
+				if (isInRole) break;
+			}
+		}
+
+		return isInRole && !isInOthers;
+	}
+}
